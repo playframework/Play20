4d0817644f5f4cede2274728f700df3f3555f649
Felipe Bonezi <1410319+felipebonezi@users.noreply.github.com>
2020-11-14 15:25:04 -0300

    Implement Filter to restrict access to allow list IP.
    Closes playframework/playframework#10483
    
    Signed-off-by: Felipe Bonezi <1410319+felipebonezi@users.noreply.github.com>

---
+++ web/play-filters-helpers/src/main/scala/play/filters/ip/AllowedIPFilter.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package play.filters.ip
+
+import javax.inject.{Inject, Provider, Singleton}
+import play.api.inject.{SimpleModule, bind}
+import play.api.mvc._
+import play.api.{Configuration, Environment, Logger}
+
+/**
+ * A filter to restrict access to IP allow list.
+ *
+ * To enable this filter, please add it to to your application.conf file using
+ * "play.filters.enabled+=play.filters.ip.AllowedIPFilter"
+ *
+ * For documentation on configuring this filter, please see the Play documentation at
+ * https://www.playframework.com/documentation/latest/AllowedIPFilter
+ */
+@Singleton
+class AllowedIPFilter @Inject()(config: AllowedIPConfiguration) extends EssentialFilter {
+
+  private val logger = Logger(getClass)
+
+  override def apply(next: EssentialAction): EssentialAction = EssentialAction { req =>
+    import play.api.libs.streams.Accumulator
+
+    if (!this.config.ipEnabled || isAllowed(req)) {
+      next(req)
+    } else if (isExcluded(req)) {
+      logger.debug(s"Not blocked because ${req.path} is an excluded path.")
+      next(req)
+    } else {
+      logger.warn(s"Forbidden to IP ${req.remoteAddress} to access ${req.path}.")
+      Accumulator.done(Results.Forbidden)
+    }
+  }
+
+  @inline
+  private[this] def isAllowed(req: RequestHeader): Boolean = {
+    this.config.allowList.contains(req.remoteAddress)
+  }
+
+  @inline
+  private[this] def isExcluded(req: RequestHeader): Boolean = {
+    this.config.excludePaths.contains(req.path)
+  }
+
+}
+
+case class AllowedIPConfiguration(
+    ipEnabled: Boolean,
+    allowList: Seq[String],
+    excludePaths: Seq[String]
+)
+
+private object IPKeys {
+  val ipEnabled          = "play.filters.ip.enabled"
+  val allowList          = "play.filters.ip.allowList"
+  val excludePaths       = "play.filters.ip.excludePaths"
+}
+
+@Singleton
+class AllowedIPConfigurationProvider @Inject()(c: Configuration, e: Environment)
+    extends Provider[AllowedIPConfiguration] {
+
+  private val logger = Logger(getClass)
+
+  lazy val get: AllowedIPConfiguration = {
+    val ipEnabled = c.getOptional[Boolean](IPKeys.ipEnabled).getOrElse(true)
+    if (!ipEnabled) {
+      logger.warn("You set AllowedIPFilter in your application.conf but it's disabled!")
+    }
+    val allowList  = c.getOptional[Seq[String]](IPKeys.allowList).getOrElse(Seq())
+    val excludePaths  = c.getOptional[Seq[String]](IPKeys.excludePaths).getOrElse(Seq())
+
+    AllowedIPConfiguration(
+      ipEnabled,
+      allowList,
+      excludePaths
+    )
+  }
+}
+
+class AllowedIPModule
+    extends SimpleModule(
+      bind[AllowedIPConfiguration].toProvider[AllowedIPConfigurationProvider],
+      bind[AllowedIPFilter].toSelf
+    )
+
+/**
+ * The allowed IP components.
+ */
+trait AllowedIPComponents {
+  def configuration: Configuration
+  def environment: Environment
+
+  lazy val allowedIPConfiguration: AllowedIPConfiguration =
+    new AllowedIPConfigurationProvider(configuration, environment).get
+  lazy val allowedIPFilter: AllowedIPFilter =
+    new AllowedIPFilter(allowedIPConfiguration)
+}
---
+++ web/play-filters-helpers/src/test/scala/play/filters/ip/AllowedIPFilterSpec.scala
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package play.filters.ip
+
+import java.net.InetAddress
+import java.security.cert.X509Certificate
+
+import com.typesafe.config.ConfigFactory
+import javax.inject.Inject
+import play.api.http.HttpFilters
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.Results._
+import play.api.mvc._
+import play.api.mvc.request.RemoteConnection
+import play.api.test.{WithApplication, _}
+import play.api.{Configuration, Environment}
+
+private[ip] class TestFilters @Inject() (allowedIPFilter: AllowedIPFilter) extends HttpFilters {
+  override def filters: Seq[EssentialFilter] = Seq(allowedIPFilter)
+}
+
+class AllowedIPFilterSpec extends PlaySpecification {
+  "AllowedIPFilter" should {
+    "accept request when IP Filter is disabled" in new WithApplication(
+      buildApp("play.filters.ip.enabled = false")
+    ) with Injecting {
+      val req    = request("/", "192.168.0.2")
+      val result = route(app, req).get
+
+      status(result) must_== OK
+    }
+
+    "accept request when IP isn't allowed but it's an excluded path" in new WithApplication(
+      buildApp("""
+                 |play.filters.ip.enabled = true
+                 |play.filters.ip.allowList = []
+                 |play.filters.ip.excludePaths = [ ""/my-excluded-path"" ]
+      """.stripMargin)
+    ) with Injecting {
+      val req    = request("/my-excluded-path", "192.168.0.2")
+      val result = route(app, req).get
+
+      status(result) must_== OK
+    }
+
+    "accept request when IP is allowed but it's an excluded path" in new WithApplication(
+      buildApp("""
+                 |play.filters.ip.enabled = true
+                 |play.filters.ip.allowList = [ ""192.167.0.3"" ]
+                 |play.filters.ip.excludePaths = [ ""/my-excluded-path"" ]
+      """.stripMargin)
+    ) with Injecting {
+      val req    = request("/my-excluded-path", "192.168.0.3")
+      val result = route(app, req).get
+
+      status(result) must_== OK
+    }
+
+    "accept request when IP is allowed" in new WithApplication(
+      buildApp("""
+                 |play.filters.ip.enabled = true
+                 |play.filters.ip.allowList = [ ""192.168.0.1"" ]
+                 |play.filters.ip.excludePaths = []
+      """.stripMargin)
+    ) with Injecting {
+      val req    = request("/my-excluded-path", "192.168.0.1")
+      val result = route(app, req).get
+
+      status(result) must_== OK
+    }
+
+    "forbidden request when IP isn't allowed" in new WithApplication(
+      buildApp("play.filters.ip.allowList = []")
+    ) with Injecting {
+      val req    = request("/", "192.168.0.2")
+      val result = route(app, req).get
+
+      status(result) must_== FORBIDDEN
+    }
+
+  }
+
+  private def request(path: String, ip: String) = {
+    FakeRequest(method = "GET", path = path)
+      .withConnection(new RemoteConnection {
+        /**
+         * The remote client's address.
+         */
+        override def remoteAddress: InetAddress = InetAddress.getByName(ip)
+
+        /**
+         * Whether or not the connection was over a secure (e.g. HTTPS) connection.
+         */
+        override def secure: Boolean = false
+
+        /**
+         * The X509 certificate chain presented by a client during SSL requests.
+         */
+        override def clientCertificateChain: Option[Seq[X509Certificate]] = Option.empty
+      })
+      .withHeaders(HOST -> "playframework.com")
+  }
+
+  private def buildApp(config: String = "") =
+    GuiceApplicationBuilder(Environment.simple())
+      .configure(Configuration(ConfigFactory.parseString(config)))
+      .load(
+        new play.api.inject.BuiltinModule,
+        new play.api.mvc.CookiesModule,
+        new play.api.i18n.I18nModule,
+        new play.filters.ip.AllowedIPModule
+      )
+      .appRoutes(app => {
+        case ("GET", "/") =>
+          val action = app.injector.instanceOf[DefaultActionBuilder]
+          action(Ok(""))
+        case ("GET", "/my-excluded-path") =>
+          val action = app.injector.instanceOf[DefaultActionBuilder]
+          action(Ok(""))
+      })
+      .overrides(
+        bind[HttpFilters].to[TestFilters]
+      )
+      .build()
+}
